87a51efb404986d7e9f6201913fde8998c5cb816
thienth32 <thienth32@gmail.com>
2017-08-07 16:37:57 +0700

    update login logic

--- l47/login.php
+++ l47/login.php
@@ -1,5 +1,38 @@
 <?php 
 if($_SERVER['REQUEST_METHOD'] == "POST"){
+	$email = isset($_POST['email']) == true ? $_POST['email'] : "";
+	$password = isset($_POST['password']) == true ? $_POST['password'] : "";
+
+	$msg = "";
+	if($email == "" || $password == ""){
+		$msg = "Dữ liệu không hợp lệ!";
+	}else{
+		$servername = '127.0.0.1';
+		$dbname = 'k30dl';
+		$dbuser = 'root';
+		$dbpassword = '123456';
+
+		try{
+			$connect = new PDO("mysql:servername=$servername;dbname=$dbname;charset=utf8", $dbuser, $dbpassword);
+		}catch(Exception $ex){
+			echo $ex->getMessage();
+			die;
+		}
+
+		$stmt = $connect->prepare("select * from users where email = :email and password = :password");
+		$stmt->bindValue(':email', $email);
+		$stmt->bindValue(':password', md5($password));
+		$stmt->execute();
+		$result = $stmt->fetch();
+		if(!$result){
+			$msg = "Sai Email/password!";
+		}else{
+			$_SESSION['userinfo'] = $result;
+			header('location: index.php');
+			die;
+		}
+	}
+
 
 }
 
@@ -14,6 +47,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
  </head>
  <body>
  	<form method="post" >
+ 		<span style="color: red;"><?= $msg ?></span>
  		<div>
  			Email: <input type="text" name="email" value="" placeholder="Email">
  		</div>
