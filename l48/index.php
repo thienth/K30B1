ffd91713f3c8d97430a37918d2276a5b9aa61dee
thienth32 <thienth32@gmail.com>
2017-08-10 14:10:00 +0700

    update

--- l48/index.php
+++ l48/index.php
@@ -1,17 +1,17 @@
 <?php 
 require_once 'Animal.php';
 
-$conBo = new Animal();
-$conBo->name = 'Bu';
-$conBo->legNumber = 4;
+$conBo = new Animal('Bu', 3, 40);
+// $conBo->name = 'Bu';
+// $conBo->legNumber = 4;
 var_dump($conBo);
-$conBo->feed(4);
-var_dump($conBo);
-$conCho = new Animal();
-$conCho->name = 'Cau Vang';
-var_dump($conCho);
-$conCho->feed(1);
-var_dump($conCho);
+// $conBo->feed(4);
+// var_dump($conBo);
+// $conCho = new Animal();
+// $conCho->name = 'Cau Vang';
+// var_dump($conCho);
+// $conCho->feed(1);
+// var_dump($conCho);
 
 // Tạo đối tượng K30DL
 // Tự tiến hành định nghĩa các thuộc tính và
