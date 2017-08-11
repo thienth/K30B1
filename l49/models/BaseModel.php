d6b63f7e2dd97c51ce3e6bfbca0c71f90a916abe
thienth32 <thienth32@gmail.com>
2017-08-11 17:29:57 +0700

    update base model

---
+++ l49/models/BaseModel.php
@@ -0,0 +1,40 @@
+<?php 
+/**
+* 
+*/
+class BaseModel
+{
+	public static function find($id = null){
+		if($id != null){
+			$query = "select * from :tablename where id = :id";
+			$conn = BaseModel::getConnect();
+			$stmt = $conn->prepare($query);
+			$stmt->bindValue(':tablename', static::$table);
+			$stmt->bindValue('id', $id);
+			$stmt->execute();
+			$rs = $stmt->fetchAll(PDO::FETCH_CLASS, static::class);
+			if(count($rs) > 0){
+				return $rs[0];
+			}
+
+			return null;
+
+		}else{
+			$model = new static();	
+			return $model;
+		}
+		
+	}
+
+	public static function getConnect(){
+		$servername = 'localhost';
+		$dbname = 'k30dl';
+		$dbUsername = 'root';
+		$dbPassword = '123456';
+		$conn = new PDO("mysql:servername=$servername;dbname=$dbname;charset=utf8;", $dbUsername, $dbPassword);
+		return $conn;
+	}
+}
+// User::find()->get();
+
+ ?>
---
+++ l49/models/UserModel.php
@@ -0,0 +1,11 @@
+<?php 
+require_once 'BaseModel.php';
+/**
+* 
+*/
+class UserModel extends BaseModel
+{
+	public static $table = 'users';
+
+}
+ ?>
