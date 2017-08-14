6eeccea8d217ba6aa57206bc03a09da868f330bf
thienth32 <thienth32@gmail.com>
2017-08-14 16:24:33 +0700

    update

--- l50/models/BaseModel.php
+++ l50/models/BaseModel.php
@@ -4,6 +4,8 @@
 */
 class BaseModel
 {
+	// Hàm mục đích tạo ra model thuộc dạng static có chứa câu query builder: select * from tên bảng where tên cột =(hoặc phép so sánh ở vị trí thứ 2) 'giá trị cần tìm kiếm'
+	// Trả về object
 	public static function where(){
 		if ( func_num_args() >= 2 ){
 			$compare = func_num_args() > 2 ? func_get_args()[1] : "=";
@@ -21,6 +23,8 @@ class BaseModel
 	    return false;
 	}
 
+	// Hàm dựa vào câu query builder của các hàm where để lấy ra toàn bộ thông tin của cần tìm trong các câu lệnh where
+	// Trả về list object/null
 	public function get(){
 		$conn = BaseModel::getConnect();
 		$stmt = $conn->prepare($this->queryBuilder);
@@ -32,6 +36,7 @@ class BaseModel
 		return $rs;
 	}
 
+	// Hàm tìm ra đối tượng có id = tham số truyền vào (trả về object)
 	public static function find($id){
 		$query = "select * from " . static::$table . " where id = :id";
 		$conn = BaseModel::getConnect();
@@ -46,6 +51,8 @@ class BaseModel
 		return null;
 	}
 
+	// Hàm tìm ra tất cả các bản ghi trong cơ sở dữ liệu của 1 bảng có model đại diện
+	// Trả về list object
 	public static function all(){
 		$query = "select * from " . static::$table;
 		$conn = BaseModel::getConnect();
@@ -57,6 +64,8 @@ class BaseModel
 		return $rs;
 	}
 
+	// Hàm tạo kết nối cơ sở dữ liệu
+	// Trả về PDO object
 	public static function getConnect(){
 		$servername = 'localhost';
 		$dbname = 'k30dl';
