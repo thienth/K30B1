60142704b0730f77a8089230f22d8fb184e61230
thienth32 <thienth32@gmail.com>
2017-08-14 16:20:40 +0700

    update

--- l50/index.php
+++ l50/index.php
@@ -3,6 +3,6 @@ require_once './models/BaseModel.php';
 require_once './models/UserModel.php';
 require_once './models/ClassesModel.php';
 
-$anhmt = UserModel::all();
+$anhmt = UserModel::where('name', 'like', '%a%')->get();
 var_dump($anhmt);
  ?>
--- l50/models/BaseModel.php
+++ l50/models/BaseModel.php
@@ -4,6 +4,34 @@
 */
 class BaseModel
 {
+	public static function where(){
+		if ( func_num_args() >= 2 ){
+			$compare = func_num_args() > 2 ? func_get_args()[1] : "=";
+			$column = func_get_args()[0];
+			$value = func_num_args() > 2 ? func_get_args()[2] : func_get_args()[1];
+
+			$model = new static();
+			$model->paramArr = [];
+			$model->queryBuilder = "select * from " 
+									. static::$table 
+									. " where $column $compare :value".count($model->paramArr);
+			$model->paramArr[] = $value;
+			return $model;
+	    }
+	    return false;
+	}
+
+	public function get(){
+		$conn = BaseModel::getConnect();
+		$stmt = $conn->prepare($this->queryBuilder);
+		for ($i=0; $i < count($this->paramArr); $i++) { 
+			$stmt->bindValue(':value'.$i, $this->paramArr[$i]);
+		}
+		$stmt->execute();
+		$rs = $stmt->fetchAll(PDO::FETCH_CLASS, static::class);
+		return $rs;
+	}
+
 	public static function find($id){
 		$query = "select * from " . static::$table . " where id = :id";
 		$conn = BaseModel::getConnect();
@@ -15,7 +43,6 @@ class BaseModel
 		if(count($rs) > 0){
 			return $rs[0];
 		}
-
 		return null;
 	}
 
