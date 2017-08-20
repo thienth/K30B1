0121f05175af4f3dc700e8cb6bd2b1b7a2d3a40a
thienth32 <thienth32@gmail.com>
2017-08-20 08:09:50 +0700

    update

--- l51/index.php
+++ l51/index.php
@@ -4,10 +4,10 @@ require_once './models/UserModel.php';
 require_once './models/ClassesModel.php';
 
 $duytd = UserModel::find(9);
-$duytd->email = 'duytd123@gmail.com';
+$duytd->email = 'duytd@gmail.com';
 $duytd->password = md5('123456');
-$duytd->name = 'Tran Dinh Duy Hung';
-$duytd->address = "Cat Ba, Hai Phong";
+$duytd->name = 'Tran Dinh Duy';
+$duytd->address = "Tien Lang, Hai Phong";
 // var_dump($duytd);die;
 echo $duytd->save();
 
