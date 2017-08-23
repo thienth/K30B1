fba43f96961ad3d42c4d6ea6b7748c216e2d384d
thienth32 <thienth32@gmail.com>
2017-08-23 16:01:39 +0700

    update homepage

--- l53/views/homepage.php
+++ l53/views/homepage.php
@@ -4,42 +4,43 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title></title>
-	<link rel="stylesheet" href="">
+	<link rel="stylesheet" href="public/plugins/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="public/css/main.css">
 </head>
 <body>
-<table>
-	<thead>
-		<tr>
-			<th>Id</th>
-			<th>Email</th>
-			<th>Name</th>
-			<th>Avatar</th>
-			<th>Address</th>
-			<th>
-				<a href="create" title="">Create</a>
-			</th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php 
-		foreach ($users as $key => $value) {
-			echo "<tr>";
-			echo "<td>$value->id</td>";
-			echo "<td>$value->email</td>";
-			echo "<td>$value->name</td>";
-			echo "<td>
-					<img src=" . $value->avatar . " width='100'>
-				</td>";
-			echo "<td>$value->address</td>";
-			echo "<td>
-				<a href='update-user?id=" . $value->id ."' >Update</a>
-				<a href='remove-user?id=" . $value->id ."' >Remove</a>
-			</td>";
-			echo "</tr>";
-		}
-		 ?>
-	</tbody>
-</table>
-	
+	<nav class="navbar navbar-default">
+	  <div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <a class="navbar-brand" href="#">Brand</a>
+	    </div>
+
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav navbar-right">
+	        <li><a href="#">Link</a></li>
+	        <li><a href="#">Link</a></li>
+	        <li><a href="#">Link</a></li>
+	        <li><a href="#">Link</a></li>
+	        <li class="dropdown">
+	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+	          <ul class="dropdown-menu">
+	            <li><a href="#">Action</a></li>
+	            <li><a href="#">Another action</a></li>
+	            <li><a href="#">Something else here</a></li>
+	            <li role="separator" class="divider"></li>
+	            <li><a href="#">Separated link</a></li>
+	          </ul>
+	        </li>
+	      </ul>
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
 </body>
 </html>
